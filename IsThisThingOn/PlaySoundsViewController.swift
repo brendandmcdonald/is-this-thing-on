e170b295cf4feb25e802b8fb5cd61bc64cc6f53e
Brendan McDonald <brendan@brendandmcdonald.com>
2015-04-04 22:19:08 -0500

    cleaned up the file

--- IsThisThingOn/PlaySoundsViewController.swift
+++ IsThisThingOn/PlaySoundsViewController.swift
@@ -11,22 +11,6 @@ import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     
-    
-    
-    // Add a button to increase the speed of playback
-        // TODO: Add the rabbit button to the correnct screen on the storyboard
-        // TODO: Add contraints to the button to have it locked to the top right of the screen
-        // TODO: Add an action to the button to connect it to our PlaySoundsViewController
-        // TODO: Make the button play the audio clip at 2.0x
-    
-    // Add a stop button to stop any audio from playing
-        // TODO: Add a stop button to the correct screen on the storyboard
-        // TODO: Add contraints to the button to lock it to the bottom center of the screen
-        // TODO: Make the button initially hidden or deactivated, but then unhides when audio is playing.
-        // TODO: Add an action to the button to connect it to our PlaySoundsViewController
-        // TODO: Have the button stop all the audio playing back
-    
-    
     var audioPlayer:AVAudioPlayer!
 
     override func viewDidLoad() {
@@ -55,36 +39,23 @@ class PlaySoundsViewController: UIViewController {
     
     func playAudio(audio: AVAudioPlayer, speed: Float){
         audio.stop()
+        // resets the audio to start from the beggining of the track. Without this, the audio started to play fast would then continue from the same point playing slow, or vice versa.
+        audioPlayer.currentTime = 0.0
         audio.rate = speed
         audio.play()
     }
     
     
     @IBAction func playAudioSlow(sender: UIButton) {
-        
-        // it's a best practice to stop the audio player before beginning to play it.
-        audioPlayer.stop()
-        audioPlayer.rate = 0.75
-        
-        // v this resets the audio so it doesn't keep playing from a different source.
-        audioPlayer.currentTime = 0.0
-        audioPlayer.play()
+       playAudio(audioPlayer, speed: 0.5)
     }
     
     @IBAction func playAudioFast(sender: UIButton) {
-        
-        // it's a best practice to stop the audio player before beginning to play it.
-        //audioPlayer.stop()
-        //audioPlayer.rate = 2.0
-        //audioPlayer.play()
-        
         playAudio(audioPlayer, speed: 2.0)
-        
     }
     
     
     @IBAction func stopAllAudio(sender: UIButton) {
-        
         audioPlayer.stop()
     }
     
